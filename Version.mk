ff89da9559b104fe59cb37f8fb41e29d2fe06e16
Kito Cheng <kito.cheng@sifive.com>
2021-01-08 20:39:03 +0800

    Bump version to 2020.12.3 for RVB 0.93

--- Version.mk
+++ Version.mk
@@ -2,4 +2,4 @@
 RISCV_TOOLCHAIN_METAL_VERSION := 10.2.0
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_TOOLCHAIN_METAL_ID := 2020.12.2
+FREEDOM_TOOLCHAIN_METAL_ID := 2020.12.3
