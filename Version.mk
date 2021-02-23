e5717b44eac9cb76116340b0ddadecb04b43f716
cgsfv <cgsfv@users.noreply.github.com>
2021-02-22 22:49:18 -0800

    Sync with main

--- Version.mk
+++ Version.mk
@@ -3,3 +3,7 @@ RISCV_TOOLCHAIN_METAL_VERSION := 10.2.0
 
 # Customization ID, which should identify the customization added to the original by SiFive
 FREEDOM_TOOLCHAIN_METAL_ID := 2020.12.8
+
+# Characteristic tags, which should be usable for matching up providers and consumers
+FREEDOM_TOOLCHAIN_METAL_RISCV_TAGS = rv32i rv64i m a f d c v zfh zba zbb
+FREEDOM_TOOLCHAIN_METAL_TOOLS_TAGS = binutils-metal gcc10-metal gdb-metal
